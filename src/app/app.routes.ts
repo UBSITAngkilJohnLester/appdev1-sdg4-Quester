2f4955c51a81b817557a3f79b7159e563862d437
UBSITAngkilJohnLester <20242643@s.ubaguio.edu>
2026-04-17 08:05:52 +0800

    About, and home components have been created along with their routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { Learn } from '../Components/learn/learn';
 import { Progit1Info } from '../Components/progit1-info/progit1-info';
 import { Sitnet1Info } from '../Components/sitnet1-info/sitnet1-info';
 import { Opsyst1Info } from '../Components/opsyst1-info/opsyst1-info';
+import { Home } from '../Components/home/home';
+import { About } from '../Components/about/about';
 
 export const routes: Routes = [
     { path: 'learn', component: Learn},
     { path: 'progit', component: Progit1Info},
     { path: 'sitnet', component: Sitnet1Info},
-    { path: 'opsyst', component: Opsyst1Info}
+    { path: 'opsyst', component: Opsyst1Info},
+    { path: 'home', component: Home},
+    { path: 'flashcards', component: Home},
+    { path: 'quizzes', component: Home},
+    { path: 'about', component: About}
 ];
