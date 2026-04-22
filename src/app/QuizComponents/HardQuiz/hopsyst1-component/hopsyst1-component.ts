e8bb376dafa865c0917b30ee8874fc90cbc9683f
Isar, Jada <20187322@s.ubaguio.edu>
2026-04-22 13:21:45 +0800

    Improved the UI in Home, Quiz page, and slight background change

--- src/app/QuizComponents/HardQuiz/hopsyst1-component/hopsyst1-component.ts
+++ src/app/QuizComponents/HardQuiz/hopsyst1-component/hopsyst1-component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hopsyst1-component',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hopsyst1-component.html',
   styleUrl: './hopsyst1-component.css',
 })
