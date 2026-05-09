9de1a52f00f8ae5840e375ade9929674c457e1b7
Dugyawe-NashCarlo <20243649@s.ubaguio.edu>
2026-05-09 16:12:05 +0800

    Finished FlashCards

--- src/app/customfcards/customfcards.ts
+++ src/app/customfcards/customfcards.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-customfcards',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './customfcards.html',
   styleUrl: './customfcards.css',
 })
